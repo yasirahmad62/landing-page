7f2c7e77c29d48910ce0435512cfcedb14aebecb
Ahmad Yasir <51242252+yasirahmad62@users.noreply.github.com>
2020-06-15 19:43:32 +0530

    Add animation

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-const spans = document.querySelectorAll('span')
-spans.forEach(span=> span.addEventListener('mouseover',function(e){
-  span.classList.add('animated','rubberband')
+const spans = document.querySelectorAll('h1 span')
+spans.forEach(span=>span.addEventListener('mouseover',function(e){
+  span.classList.add('animated','rubberBand')
 }))
-spans.forEach(span=> span.addEventListener('mouseout',function(e){
-  span.classList.remove('animated',"rubberBand")
+spans.forEach(span=>span.addEventListener('mouseout',function(e){
+  span.classList.remove('animated','rubberBand')
 }))
-const showRequiredCategory = event =>{
-  const getId = event.id
-  const links = document.querySelectorAll(".work-category button")
-  for(i = 0;i<links.lenght;i++){
-    if (links[i].hasAttribute('class')){
-      links[i].classList.remove('active')
-    }
-  }
-  event.classList.add("active")
-  const getCategory = document.querySelector('.category-${getid}')
-  const categories = document.querySelectorAll('div[class ^="category-"]')
-  for(i = 0;i<categories.lenght;i++){
-    if (categories[i].hasAttribute('class')){
-      categories[i].classList.remove('showCategory')
-      categories[i].classList.add('hideCategory')
-    }
-  }
-  getCategory.classList.remove("hideCategory ")
-  getCategory.classList.add("showCategory ")
-}
+
+//FOR ADDING Features
+// const showRequiredCategory = event =>{
+//   const getId = event.id
+//   const links = document.querySelectorAll(".work-category button")
+//   for(i = 0;i<links.lenght;i++){
+//     if (links[i].hasAttribute('class')){
+//       links[i].classList.remove('active')
+//     }
+//   }
+//   event.classList.add("active")
+//   const getCategory = document.querySelector('.category-${getid}')
+//   const categories = document.querySelectorAll('div[class ^="category-"]')
+//   for(i = 0;i<categories.lenght;i++){
+//     if (categories[i].hasAttribute('class')){
+//       categories[i].classList.remove('showCategory')
+//       categories[i].classList.add('hideCategory')
+//     }
+//   }
+//   getCategory.classList.remove("hideCategory ")
+//   getCategory.classList.add("showCategory ")
+// }
